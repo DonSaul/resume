be851ecd1a7c4d2304b0355358315bef25a5225e
saul olguin <sauloainc@gmail.com>
2020-07-08 16:14:37 -0500

    cursor change

--- public/main.js
+++ public/main.js
@@ -1,1 +1,12 @@
 
+var cursor = true;
+var speed = 250;
+setInterval(() => {
+  if(cursor) {
+    document.getElementById('cursor').style.opacity = 0;
+    cursor = false;
+  }else {
+    document.getElementById('cursor').style.opacity = 1;
+    cursor = true;
+  }
+}, speed);
